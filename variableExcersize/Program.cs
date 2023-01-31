af405a71a7273e11fc8fce3e998f15b0e62ff64d
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-01-30 19:08:18 -0600

    variables excersize

---
+++ variableExcersize/Program.cs
@@ -0,0 +1,13 @@
+﻿// Declaring and initializing variables
+string myName = "Giovanni";
+int age = 2;
+char firstInitial ='G';
+bool isover2 = false;
+double height = 38.2;
+decimal weight = 25.87m;
+
+Console.WriteLine($" {firstInitial} is for {myName}.\n" +
+                  $" is {myName} over the age of 2 ? {isover2}\n" +
+                  $" {myName} is {age} years old. \n" +
+                  $" he is {height} inches tall. \n" +
+                  $" and weighs {weight} pounds.");
